aa1243987b8ed5565f9d7102ab0ec86b21b50483
mzabriskie <mzabriskie@gmail.com>
2015-04-03 19:19:03 -0600

    Adding intro/game over screen

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
+(function () {
+
 var app = {
-  state: 1,
+  state: 0,
   score: 0,
   explosionTimer: 0,
 
@@ -8,54 +10,67 @@ var app = {
   STATE_END: 0
 };
 
-function startGame() {
-  app.canvas = document.getElementById("canvas");
-  app.ctx = app.canvas.getContext("2d");
+(function initGame() {
+  var canvas = document.getElementById("canvas");
+  app.ctx = canvas.getContext("2d");
 
   scaleCanvas();
 
-  // load images
-  // http://www.over00.com/index.php/archives/1844
+  // load assets
   app.shipImage = new Image();
-  app.shipImage.src = "images/ship.png";
+  app.shipImage.src = "images/ship.png"; // http://www.over00.com/index.php/archives/1844
 
-  // http://www.zimnox.com/resources/textures/
   app.rockImage = new Image();
-  app.rockImage.src = "images/rock.png";
+  app.rockImage.src = "images/rock.png"; // http://www.zimnox.com/resources/textures/
 
-  // http://dbszabo1.deviantart.com/art/misc-explosion-element-png-309933232
   app.explosionImage = new Image();
-  app.explosionImage.src = "images/explosion.png";
-
-  // load audio
-  // https://www.freesound.org/people/ryansnook/sounds/110113/
+  app.explosionImage.src = "images/explosion.png"; // http://dbszabo1.deviantart.com/art/misc-explosion-element-png-309933232
+  
   app.explosionSound = new Audio();
-  app.explosionSound.src = "audio/explosion.wav";
+  app.explosionSound.src = "audio/explosion.wav"; // https://www.freesound.org/people/ryansnook/sounds/110113/
 
-  // http://www.playonloop.com/2012-music-loops/gunman/
   app.themesongSound = new Audio();
-  app.themesongSound.src = "audio/gunman.wav";
+  app.themesongSound.src = "audio/gunman.wav"; // http://www.playonloop.com/2012-music-loops/gunman/
   app.themesongSound.loop = true;
   app.themesongSound.play();
-
-  // list of objects
-  app.objects = [];
-  
-  // create hero and random rocks
-  spawnHero();
-  spawnAllRocks();
 
   // register events
   canvas.addEventListener("mousemove", handleMouseMove, false);
   window.addEventListener("resize", handleWindowResize, false);
-  
+  document.addEventListener("keypress", handleDocumentKeypress, false);
+
   // animation loop
   animationLoop();
+})();
+
+function startGame() {
+  clearCanvas();
+
+  // reset state
+  app.state = app.STATE_PLAY;
+  app.score = 0;
+  app.explosionTimer = 0;
+  app.explosionSound.pause();
+  app.explosionSound.currentTime = 0;
+
+  // list of objects
+  app.objects = [];
+  
+  // create hero and world objects
+  spawnHero();
+  spawnStars();
+  spawnAllRocks();
+}
+
+function clearCanvas() {
+  var ctx = app.ctx;
+  ctx.fillStyle = "#000020";
+  ctx.fillRect(0, 0, app.width, app.height);
 }
 
 function scaleCanvas() {
-  app.width = app.canvas.width = window.innerWidth;
-  app.height = app.canvas.height = window.innerHeight;
+  app.width = app.ctx.canvas.width = window.innerWidth;
+  app.height = app.ctx.canvas.height = window.innerHeight;
 }
 
 function animationLoop() {
@@ -64,6 +79,11 @@ function animationLoop() {
 
 function frameUpdate(timestamp) {
   animationLoop();
+
+  if (app.state === app.STATE_END && app.score === 0) {
+    drawIntro();
+    return;
+  }
 
   // delta time calculation
   if (!app.lastTimeStamp) {
@@ -124,14 +144,55 @@ function frameUpdate(timestamp) {
 
   // redraw everything
   drawScene();
+
+  // draw game over
+  if (app.state === app.STATE_END) {
+    drawGameOver();
+  }
+}
+
+function drawIntro() {
+  clearCanvas();
+
+  var ctx = app.ctx;
+
+  // draw name
+  ctx.textAlign = "center";
+  ctx.fillStyle = "#00ffff";
+  ctx.font = "130px Calibri";
+  ctx.fillText("Asteroid Evader", app.width/2, app.height/4);
+
+  // draw instructions
+  ctx.textAlign = "center";
+  ctx.fillStyle = "#fff";
+  ctx.font = "20px Calibri";
+  ctx.fillText("Use mouse to navigate ship through asteroid field", app.width/2, app.height/2);
+
+  // draw start
+  ctx.textAlign = "center";
+  ctx.fillStyle = "#fff";
+  ctx.font = "italic 20px Calibri";
+  ctx.fillText("(press any key to play)", app.width/2, app.height/2+50);
+}
+
+function drawGameOver() {
+  var ctx = app.ctx;
+  ctx.textAlign = "center";
+  ctx.fillStyle = "#00ffff";
+  ctx.font = "italic 130px Calibri";
+  ctx.fillText("Game Over", app.width/2, app.height/2);
+
+  // draw start
+  ctx.textAlign = "center";
+  ctx.fillStyle = "#fff";
+  ctx.font = "italic 20px Calibri";
+  ctx.fillText("(press any key to play again)", app.width/2, app.height/2+50);
 }
 
 function drawScene() {
-  var ctx = app.ctx;
+  clearCanvas();
 
-  // fill background
-  ctx.fillStyle = "#000020";
-  ctx.fillRect(0, 0, app.width, app.height);
+  var ctx = app.ctx;
 
   // draw the explosion
   if (app.explosionTimer > 0) {
@@ -153,30 +214,50 @@ function drawScene() {
     }
 
     ctx.save();
+    if (o.type === "star") {
+      ctx.beginPath();
+      ctx.globalAlpha = o.brightness;
+      ctx.fillStyle = o.color;
+      ctx.arc(o.pos.x, o.pos.y, o.radius, 0, Math.PI * 2, true);
+      ctx.fill();
+      ctx.closePath();
+    } else {
       ctx.translate(o.pos.x, o.pos.y);
       ctx.rotate(o.angle);
       ctx.drawImage(o.image, -o.size/2, -o.size/2, o.size, o.size);
+    }
     ctx.restore();
   }
 
   // draw score
   ctx.textAlign = "center";
-  ctx.fillStyle = "#00ffff";
+  ctx.fillStyle = "#fff";
   ctx.font = "italic 30px Calibri";
   ctx.fillText("Score " + Math.floor(app.score), app.width/2, 30);
+}
 
-  if (app.state === app.STATE_END) {
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#00ffff";
-    ctx.font = "italic 130px Calibri";
-    ctx.fillText("Game Over", app.width/2, app.height/2);
+function spawnStars() {
+  // http://codepen.io/iblamefish/pen/xgefG?editors=001
+  var STAR_COLOURS = ["#ffffff", "#ffe9c4", "#d4fbff"];
+  function random(min, max) {
+    return Math.round((Math.random() * max - min) + min);
+  }
+
+  for (var i=0; i<500; i++) {
+    app.objects.push({
+      type: "star",
+      pos: {x: Math.random() * app.width, y: Math.random() * app.height},
+      radius: Math.random() * 1.1,
+      brightness: random(80, 100) / 100,
+      color: STAR_COLOURS[random(0, STAR_COLOURS.length)]
+    });
   }
 }
 
 function spawnHero() {
   app.hero = {
     type: "hero",
-    pos: {x:400, y:500},
+    pos: {x:app.width/2, y:app.height-100},
     roll: 0,
     angle: 0,
     size: 90,
@@ -186,7 +267,7 @@ function spawnHero() {
 }
 
 function spawnRock() {
-  var o = {
+  app.objects.push({
     type: "rock",
     pos: {x:Math.random() * app.width, y:Math.random() * -app.height},
     angle: Math.random() * Math.PI * 2,
@@ -194,8 +275,7 @@ function spawnRock() {
     speed: Math.random() * 150 + 100,
     size: 90,
     image: app.rockImage
-  };
-  app.objects.push(o);
+  });
 }
 
 function spawnAllRocks() {
@@ -210,10 +290,18 @@ function handleMouseMove(e) {
     return;
   }
 
-  app.hero.pos.x = e.pageX - app.canvas.offsetLeft;
-  app.hero.pos.y = e.pageY - app.canvas.offsetTop;
+  app.hero.pos.x = e.pageX - app.ctx.canvas.offsetLeft;
+  app.hero.pos.y = e.pageY - app.ctx.canvas.offsetTop;
 }
 
 function handleWindowResize(e) {
   scaleCanvas();
 }
+
+function handleDocumentKeypress(e) {
+  if (app.state == app.STATE_END) {
+    startGame();
+  }
+}
+
+})();
